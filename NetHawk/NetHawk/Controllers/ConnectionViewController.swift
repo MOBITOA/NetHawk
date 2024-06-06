e3c7aa8db1d8e849f0b7e25a805f3ddbfa4c42a5
giljihun <accc45@outlook.com>
2024-06-06 16:04:55 +0900

    add : Splash, ConnectionView

---
+++ NetHawk/NetHawk/Controllers/ConnectionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectionViewController.swift
+//  NetHawk
+//
+//  Created by mobicom on 6/6/24.
+//
+
+import UIKit
+
+class ConnectionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
